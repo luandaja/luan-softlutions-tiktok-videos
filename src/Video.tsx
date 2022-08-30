2dce135161cb0481d1dd8789e6ac08c45e48ec00
Luis Anroni <luandaja>
2022-08-30 10:31:22 -0500

    remove MUI

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,31 +1,13 @@
-import './style.css';
-
-import React from 'react';
-
 import { DesignPatternsComposition } from '@compositions/DesignPatterns/DesignPatternsComposition';
 import { TiktokTestComposition } from '@compositions/TiktokTest/TiktokTestComposition';
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-
+import React from 'react';
+import './style.css';
 
 export const RemotionVideo: React.FC = () => {
-	const theme = createTheme({
-		breakpoints: {
-			values: {
-				xs: 1080,
-				sm: 600,
-				md: 900,
-				lg: 1200,
-				xl: 1536,
-			},
-		},
-	});
 	return (
 		<>
-			<ThemeProvider theme={theme}>
-
-				<DesignPatternsComposition></DesignPatternsComposition>
-				<TiktokTestComposition></TiktokTestComposition>
-			</ThemeProvider>
+			<DesignPatternsComposition></DesignPatternsComposition>
+			<TiktokTestComposition></TiktokTestComposition>
 		</>
 	);
 };
--- src/compositions/TiktokTest/TiktokTestComposition.tsx
+++ src/compositions/TiktokTest/TiktokTestComposition.tsx
@@ -1,55 +1,38 @@
-import { Tiktok } from "@components/compositions"
-import { TitleSequence } from "@components/sequences/TitleSequence"
-import { Alert, Avatar, Stack } from "@mui/material"
-import { Img, Sequence, staticFile } from "remotion"
-
-import React from 'react'
-
-
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
+import { Tiktok } from '@components/compositions';
+import { TitleSequence } from '@components/sequences/TitleSequence';
+import { Avatar } from '@mui/material';
+import React from 'react';
+import { Img, Sequence, staticFile } from 'remotion';
 
 export const TiktokTestComposition = () => {
-  return (
-    <Tiktok.Still
-      durationInFrames={3000}
-      id="Tiktok-Test"
-      component={Test}
-    ></Tiktok.Still>
-  )
-}
+	return <Tiktok.Still durationInFrames={3000} id="Tiktok-Test" component={Test}></Tiktok.Still>;
+};
 
 export const Test = () => {
-  const tiktokSafeZoneImage = staticFile("/feed_1.png")
-  return <>
-    <Sequence from={0}>
-
-      <Img src={tiktokSafeZoneImage}></Img>
-    </Sequence>
-    {/* <Sequence from={0}>
+	const tiktokSafeZoneImage = staticFile('/feed_1.png');
+	return (
+		<>
+			<Sequence from={0}>
+				<Img src={tiktokSafeZoneImage}></Img>
+			</Sequence>
+			{/* <Sequence from={0}>
       <div className="container px-32 pt-56 pb-144 opacity-90">
         <div className="bg-gray-100 w-full h-full "></div>
       </div>
     </Sequence> */}
 
-    <TitleSequence from={0} animated={true} to={200} className="self-start text-white">
-      Esto es un test muy largo
-    </TitleSequence>
+			<TitleSequence from={0} animated={true} to={200} className="self-start text-white">
+				Esto es un test muy largo
+			</TitleSequence>
 
-    <TitleSequence from={100} animated={true} durationInFrames={100} className="text-red-400 self-center">
-      Esto es un test muy largo
-    </TitleSequence>
-    <TitleSequence from={0} animated={true} to={200} className=" text-amber-500 self-end">
-      Esto es un test muy largo
-    </TitleSequence>
+			<TitleSequence from={100} animated={true} durationInFrames={100} className="text-red-400 self-center">
+				Esto es un test muy largo
+			</TitleSequence>
+			<TitleSequence from={0} animated={true} to={200} className=" text-amber-500 self-end">
+				Esto es un test muy largo
+			</TitleSequence>
 
-    <Avatar >H</Avatar>
-
-
-  </>
-}
+			<Avatar>H</Avatar>
+		</>
+	);
+};
