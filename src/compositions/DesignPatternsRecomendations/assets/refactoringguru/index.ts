8ba4ab64294282fd4dc6200b651f04106a3ceeb4
Luis Anroni <luandaja>
2022-08-30 22:08:26 -0500

    remove legacy video

--- src/compositions/DesignPatternsRecomendations/assets/refactoringguru/index.ts
+++
@@ -1,8 +0,0 @@
-// @index('./*.{png,jpg,svg}', (f, _) => `export { default as img${_.pascalCase(f.name)} } from '${f.path}${f.ext}'`)
-export { default as imgAplications } from './aplications.png';
-export { default as imgEnespanOl } from './enespañol.png';
-export { default as imgIntro } from './intro.png';
-export { default as imgLibro } from './libro.png';
-export { default as imgList } from './list.png';
-export { default as imgProblem } from './problem.png';
-export { default as imgPurprose } from './purprose.png';
--- src/compositions/DesignPatternsRecomendations/sequences/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './Intro';
-export * from './Outro';
-export * from './RecomendationDoFactory';
-export * from './RecomendationPatternsDev';
-export * from './RecomendationRefactoringGuru';
